3b8a324b82124c64557e9d21b74e37c44f54f5f8
Fábio Santos Villar <63696039+FabioVillar@users.noreply.github.com>
2023-04-22 23:32:45 +0000

    Small improv in server

--- server.c
+++ server.c
@@ -9,6 +9,84 @@
 #define MAX 80
 #define PORT 8080
 #define SA struct sockaddr
+
+char* readFromClient(char* buff, int connfd){
+	bzero(buff, MAX);
+	// read the message from client and copy it in buffer
+	read(connfd, buff, sizeof(buff));
+	printf("Client send this message: %s", buff);
+	return buff;
+}
+void sendToClient(char* buff, int connfd){
+	printf("Is gonna send something\n");
+	printf("%s\n", buff);
+	int n = 0;
+	// copy server message in the buffer
+	while ((buff[n++] = getchar()) != '\n')
+		;
+	// and send that buffer to client
+	printf("To client: %s", buff);
+	write(connfd, buff, sizeof(buff));
+	// if msg contains "Exit" then server exit and chat ended.
+	if (strncmp("exit", buff, 4) == 0) {
+		printf("Server Exit...\n");
+	}
+}
+void insertProfile(char* buff, int connfd){
+	FILE* fp;
+	fp = fopen("profile.txt", "w");
+	fprintf(fp, "Profile:");
+	strcpy(buff, "Insert-email");
+	sendToClient(buff, connfd);
+	strcpy(buff, readFromClient(buff, connfd));
+	char email[50] = "";
+	strcpy(email, buff);
+
+	strcpy(buff, "Insert name");
+	sendToClient(buff, connfd);
+	strcpy(buff, readFromClient(buff, connfd));
+	char name[50] = "";
+	strcpy(name, buff);
+
+	strcpy(buff, "Insert last name");
+	sendToClient(buff, connfd);
+	strcpy(buff, readFromClient(buff, connfd));
+	char lastName[50] = "";
+	strcpy(lastName, buff);
+
+	strcpy(buff, "Insert your city");
+	sendToClient(buff, connfd);
+	strcpy(buff, readFromClient(buff, connfd));
+	char city[50] = "";
+	strcpy(city, buff);
+
+	strcpy(buff, "Insert your graduation field");
+	sendToClient(buff, connfd);
+	strcpy(buff, readFromClient(buff, connfd));
+	char graduationField[50] = "";
+	strcpy(graduationField, buff);
+
+	strcpy(buff, "Insert graduation year");
+	sendToClient(buff, connfd);
+	strcpy(buff, readFromClient(buff, connfd));
+	char year[5] = "";
+	strcpy(year, buff);
+
+	strcpy(buff, "Insert your skills");
+	sendToClient(buff, connfd);
+	strcpy(buff, readFromClient(buff, connfd));
+	char skills[100] = "";
+	strcpy(skills, buff);
+
+	fprintf(fp, "%s",  email);
+
+	fclose(fp);
+}
+void manageBuff(char* buff, int connfd){
+	if(buff[0]=='1'){
+		insertProfile(buff, connfd);
+	}
+}
 // Function designed for chat between client and server.
 void func(int connfd)
 {
@@ -16,25 +94,8 @@ void func(int connfd)
 	int n;
 	// infinite loop for chat
 	for (;;) {
-		bzero(buff, MAX);
-		// read the message from client and copy it in buffer
-		read(connfd, buff, sizeof(buff));
-        printf("Client send this message: %s", buff);
-		manageBuff(buff);
-		// print buffer which contains the client contents
-		printf("From client: %s\t To client : ", buff);
-		bzero(buff, MAX);
-		n = 0;
-		// copy server message in the buffer
-		while ((buff[n++] = getchar()) != '\n')
-			;
-		// and send that buffer to client
-		write(connfd, buff, sizeof(buff));
-		// if msg contains "Exit" then server exit and chat ended.
-		if (strncmp("exit", buff, 4) == 0) {
-			printf("Server Exit...\n");
-			break;
-		}
+		strcpy(readFromClient(buff, connfd), buff);
+		manageBuff(buff, connfd);
 	}
 }
 
